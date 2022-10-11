43a2addcf4e416212d280ee876d163585100666f
caokun <2524039650@qq.com>
2022-10-11 11:36:37 +0800

    second commit

---
+++ data/dataset.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+import torch
+from data.voc_dataset import VOCBboxDataset
+from skimage import transform as sktsf
+from torchvision import transforms as tvtsf
+from data import util
+import numpy as np
+from utils.config import opt
+
+def inverse_normalize(img):
+    if opt.caffe_pretrain:
+        img = img + (np.array([122.7717, 115.9465, 102.9801]).reshape(3, 1, 1))
+        return img[::-1, :, :]
+    # approximate un-normalize for visualize
+    return (img * 0.225 + 0.45).clip(min=0, max=1) * 255
+
+def
